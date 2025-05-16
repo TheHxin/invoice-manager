4fdb09fdd7065c6377b8f8f6e663414e922251e4
deb <hxin.radin@gmail.com>
2025-05-16 22:22:39 +0200

    Update test uset model [added fullname and email to userbase]

--- test/models/user.py
+++ test/models/user.py
@@ -3,6 +3,8 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel):
     id : int = Field(default=None, primary_key=True)
     username : str = Field(default=None, index=True, unique=True)
+    full_name : str = Field(default=None)
+    email : str = Field(default=None)
 
 class User(UserBase, table = True):
     password_hashed : str = Field(default=None)
