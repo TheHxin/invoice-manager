ad5a7252af1b840298ee02810283cf01c525b9e8
deb <hxin.radin@gmail.com>
2025-07-19 19:16:58 +0200

    Update initdb.py [creates a root user]

--- source/initdb.py
+++ source/initdb.py
@@ -1,7 +1,8 @@
 import os
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 from models.invoice import *
 from models.user import *
+from auth import hashPassword
 
 if input("Delete all data? ") == "":
 	try:
@@ -12,6 +13,21 @@ if input("Delete all data? ") == "":
 	args = {"check_same_thread": False}
 	engine = create_engine(sql_url, connect_args=args)
 	SQLModel.metadata.create_all(engine)
+
+	with Session(engine) as session:
+		root_user = User(
+			username="root",
+			full_name="root",
+			email="root",
+			password_hashed=hashPassword("root1234")
+		)
+		session.add(root_user)
+		session.commit()
+		session.refresh(root_user)
+
+		print("Root user created: ")
+		print(root_user.dict())
+
 	print("db reinitiated")
 else:
 	print("abort")
