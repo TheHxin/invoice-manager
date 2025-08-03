88716b932b15ccc95b3543611344267a983a1fca
deb <hxin.radin@gmail.com>
2025-08-03 22:46:10 +0200

    Added CORS middleware for sepration of the api and the front-end server + development testing

--- source/main.py
+++ source/main.py
@@ -7,27 +7,41 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from database import *
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def initDB():
     SQLModel.metadata.create_all(engine)
 
-@asynccontextmanager 
-async def lifespan(app : FastAPI): # runs the code before "yeild" before app startup and runs the code after yeild at shutdown
-    initDB() #startup code here
-     
+
+@asynccontextmanager
+async def lifespan(
+    app: FastAPI,
+):  # runs the code before "yeild" before app startup and runs the code after yeild at shutdown
+    initDB()  # startup code here
+
     yield
 
-    #shutdown code here
+    # shutdown code here
+
 
 app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory="static"), name = "static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def index():
     return FileResponse("static/invoice_1.html")
-
 
 
 app.include_router(auth_router)
